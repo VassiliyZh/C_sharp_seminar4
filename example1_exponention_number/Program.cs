8c66df3343b11d27f03700e56c896931bf632457
VassiliyZh <111jvv@mail.ru>
2023-01-17 09:49:41 +0600

    Возведение числа А в степень В

---
+++ example1_exponention_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вводим 2 числа А и В, на выходе получаем результат возведения числа А в степень В
+
+int GetNumberFromConsole (string message) //<= отвечает за ввод данных и пишет что вводить
+    {
+        System.Console.Write(message);
+        int value = Convert.ToInt32(Console.ReadLine());
+        return value;
+    }
+int ExponentiationNumber(int A, int B) //<= данная функция делает расчет
+    {
+        int result = Convert.ToInt32(Math.Pow(A,B));
+        return result;
+    }
+int A = GetNumberFromConsole("A = ");
+int B = GetNumberFromConsole("B = ");
+
+System.Console.WriteLine(ExponentiationNumber(A,B));
