49f5716908a99ddba31a7ffb310bbd5d5927aa54
VassiliyZh <111jvv@mail.ru>
2023-01-21 23:09:15 +0600

    Складываем цифры введенного числа

---
+++ example2_summ_digit_in_number/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вводим число и на выходе получаем сумму цифр этого числа
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int[] arr = new int[num.ToString().Length];
+int i = 0, s = 0; ;
+while (num > 0)
+{
+   arr[i] = num % 10;
+   num /= 10;
+   s += arr[i];
+   i++;
+}
+Console.WriteLine("Сумма цифр числа равна " + s);
+
